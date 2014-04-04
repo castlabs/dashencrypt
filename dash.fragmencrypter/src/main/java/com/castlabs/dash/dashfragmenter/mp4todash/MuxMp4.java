4c50c8db509731fb8087e38e7bab221d558d4f5d
sannies <Sebastian.Annies@googlemail.com>
2014-04-04 10:33:27 +0200

    improve muxing to support h264 update to isoparser 1.0.0

--- dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/mp4todash/MuxMp4.java
+++ dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/mp4todash/MuxMp4.java
@@ -13,10 +13,7 @@ import com.googlecode.mp4parser.authoring.Movie;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
 import com.googlecode.mp4parser.authoring.builder.Mp4Builder;
-import com.googlecode.mp4parser.authoring.tracks.AACTrackImpl;
-import com.googlecode.mp4parser.authoring.tracks.AC3TrackImpl;
-import com.googlecode.mp4parser.authoring.tracks.DTSTrackImpl;
-import com.googlecode.mp4parser.authoring.tracks.EC3TrackImpl;
+import com.googlecode.mp4parser.authoring.tracks.*;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.spi.FileOptionHandler;
@@ -26,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.WritableByteChannel;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -34,28 +32,22 @@ public class MuxMp4 implements Command {
     @Argument(required = true, multiValued = true, handler = FileOptionHandler.class, usage = "Bitstream input files", metaVar = "vid1.avc1, aud1.dtshd ...")
     protected List<File> inputFiles;
 
-    @Option(name = "--outputdir", aliases = "-o",
-            usage = "output directory - if no output directory is given the " +
-                    "current working directory is used.",
+    @Option(name = "--outputfile", aliases = "-o",
+            usage = "output file - if no output file is given output.mp4 is used",
             metaVar = "PATH")
-    protected File outputDirectory = new File("");
+    protected File outputFile = new File("output.mp4");
 
 
     @Override
     public int run() throws IOException {
-        if (!(outputDirectory.exists() ^ outputDirectory.mkdirs())) {
-            System.err.println("Output directory does not exist and cannot be created.");
-        }
 
         long start = System.currentTimeMillis();
 
-        Map<Track, String> trackOriginalFilename = createTracks();
+        List<Track> tracks = createTracks();
 
-        // generate filenames for later reference
-        Map<Track, String> trackFilename = generateFilenames(trackOriginalFilename);
 
         // export the dashed single track MP4s
-        Map<Track, Container> mp4Files = writeSingleTrackMp4s(trackFilename);
+        writeMp4(tracks);
 
         System.out.println("Finished write in " + (System.currentTimeMillis() - start) + "ms");
         return 0;
@@ -65,30 +57,22 @@ public class MuxMp4 implements Command {
         return new DefaultMp4Builder();
     }
 
-    private HashMap<Track, Container> writeSingleTrackMp4s(Map<Track, String> filenames) throws IOException {
+    private void writeMp4(List<Track> tracks) throws IOException {
+        Movie m = new Movie();
+        m.setTracks(tracks);
 
-        HashMap<Track, Container> containers = new HashMap<Track, Container>();
-        for (Track track : filenames.keySet()) {
-            String filename = filenames.get(track);
-            Movie movie = new Movie();
-            movie.addTrack(track);
+        Mp4Builder mp4Builder = getFileBuilder(m);
+        Container isoFile = mp4Builder.build(m);
 
-            System.out.print("Creating model for " + filename + "... ");
-            Mp4Builder mp4Builder = getFileBuilder(movie);
-            Container isoFile = mp4Builder.build(movie);
+        System.out.print("Writing... ");
+        WritableByteChannel wbc = new FileOutputStream(outputFile).getChannel();
+        try {
+           isoFile.writeContainer(wbc);
 
-            System.out.print("Writing... ");
-            WritableByteChannel wbc = new FileOutputStream(
-                    new File(outputDirectory, filename)).getChannel();
-            try {
-                isoFile.writeContainer(wbc);
-                containers.put(track, isoFile);
-            } finally {
-                wbc.close();
-            }
-            System.out.println("Done.");
+        } finally {
+            wbc.close();
         }
-        return containers;
+        System.out.println("Done.");
     }
 
     /**
@@ -102,6 +86,7 @@ public class MuxMp4 implements Command {
             String originalFilename = trackOriginalFilename.get(track);
             originalFilename = originalFilename.replace(".aac", "");
             originalFilename = originalFilename.replace(".ec3", "");
+            originalFilename = originalFilename.replace(".h264", "");
             originalFilename = originalFilename.replace(".ac3", "");
             originalFilename = originalFilename.replace(".dtshd", "");
             filenames.put(track, String.format("%s.mp4", originalFilename));
@@ -110,32 +95,34 @@ public class MuxMp4 implements Command {
     }
 
     /**
-     * Creates a Map with Track as key and originating filename as value.
+     * Creates a List of Track
      *
-     * @return Track too originating file map
      * @throws java.io.IOException
      */
-    Map<Track, String> createTracks() throws IOException {
-        HashMap<Track, String> track2File = new HashMap<Track, String>();
+    List<Track> createTracks() throws IOException {
+        List<Track> tracks = new LinkedList<Track>();
         for (File inputFile : inputFiles) {
             if (inputFile.getName().endsWith(".aac")) {
                 Track track = new AACTrackImpl(new FileDataSourceImpl(inputFile));
-                track2File.put(track, inputFile.getName());
+                tracks.add(track);
             } else if (inputFile.getName().endsWith(".ac3")) {
                 Track track = new AC3TrackImpl(new FileDataSourceImpl(inputFile));
-                track2File.put(track, inputFile.getName());
+                tracks.add(track);
+            } else if (inputFile.getName().endsWith(".h264")) {
+                Track track = new H264TrackImpl(new FileDataSourceImpl(inputFile));
+                tracks.add(track);
             } else if (inputFile.getName().endsWith(".ec3")) {
                 Track track = new EC3TrackImpl(new FileDataSourceImpl(inputFile));
-                track2File.put(track, inputFile.getName());
+                tracks.add(track);
             } else if (inputFile.getName().endsWith(".dtshd")) {
                 Track track = new DTSTrackImpl(new FileDataSourceImpl(inputFile));
-                track2File.put(track, inputFile.getName());
+                tracks.add(track);
             } else {
                 System.err.println("Cannot identify type of " + inputFile + ". Extensions aac, ac3, ec3 or dtshd are known.");
             }
         }
 
-        return track2File;
+        return tracks;
     }
 
 }
