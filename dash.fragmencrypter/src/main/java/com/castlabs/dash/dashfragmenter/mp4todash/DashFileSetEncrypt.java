1a56d754c09565b5da20eb6bb978bfe2ce8deb5b
Sebastian Annies <Sebastian.Annies@castlabs.com>
2014-06-30 16:17:26 +0200

    generated keys as pskc

--- dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/mp4todash/DashFileSetEncrypt.java
+++ dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/mp4todash/DashFileSetEncrypt.java
@@ -13,19 +13,31 @@ import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.builder.FragmentIntersectionFinder;
 import com.googlecode.mp4parser.authoring.builder.Mp4Builder;
 import com.googlecode.mp4parser.util.UUIDConverter;
+import ietfParamsXmlNsKeyprovPskc.*;
 import mpegDashSchemaMpd2011.MPDDocument;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.xmlbeans.XmlBase64Binary;
 import org.apache.xmlbeans.XmlOptions;
 import org.kohsuke.args4j.Option;
+import org.w3.x2000.x09.xmldsig.KeyInfoType;
+import org.w3.x2000.x09.xmldsig.X509DataType;
+import org.w3.x2001.x04.xmlenc.CipherDataType;
+import org.w3.x2001.x04.xmlenc.EncryptedDataType;
 
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.*;
 
 
 public class DashFileSetEncrypt extends DashFileSet {
@@ -54,12 +66,19 @@ public class DashFileSetEncrypt extends DashFileSet {
     )
     String encKeySecretKeyFile = null;
 
+    @Option(name = "--certificate", aliases = "-c", usage = "X509 certificate for generation of KDF documents")
+    List<File> certificates = new LinkedList<File>();
+
     @Override
     public int run() throws IOException {
         if (((this.encKeySecretKey == null) && (this.encKeySecretKeyFile == null))) {
-            System.out.println("Please specify --secretKey or --secretKeyFile ");
-            // --uuid requirements is done via cmd line parser
-            return 1236;
+            byte[] k = new byte[16];
+            SecureRandom random = new SecureRandom();
+            random.nextBytes(k);
+            this.key = new SecretKeySpec(k, "AES");
+            if (keyid == null) {
+                this.keyid = UUID.randomUUID();
+            }
         } else {
             this.keyid = UUID.fromString(this.encKid);
             if (this.encKeySecretKey != null) {
@@ -69,9 +88,85 @@ public class DashFileSetEncrypt extends DashFileSet {
             }
         }
         super.run();
+        createKdf();
 
         return 0;
     }
+
+    protected void createKdf() {
+        // this.outputDirectory, "kdf.pskcxml"
+        try {
+            List<X509Certificate> certObjects = new LinkedList<X509Certificate>();
+            CertificateFactory cf = CertificateFactory.getInstance("X509");
+            for (File certificate : certificates) {
+                FileInputStream fis = new FileInputStream(certificate);
+                try {
+                    certObjects.add((X509Certificate) cf.generateCertificate(fis));
+                } catch (CertificateException e) {
+                    IOUtils.closeQuietly(fis);
+                    throw e;
+                }
+            }
+
+            for (X509Certificate certificate : certObjects) {
+                KeyContainerDocument keyContainerDocument = KeyContainerDocument.Factory.newInstance();
+
+                KeyContainerType keyContainer = keyContainerDocument.addNewKeyContainer();
+                keyContainer.setVersion("1.0");
+
+
+                KeyInfoType keyInfoType = keyContainer.addNewEncryptionKey();
+                X509DataType x509DataType = keyInfoType.addNewX509Data();
+                XmlBase64Binary xmlBase64BinaryCertificate = x509DataType.addNewX509Certificate();
+                xmlBase64BinaryCertificate.setByteArrayValue(certificate.getEncoded());
+
+                KeyPackageType keyPackage = keyContainer.addNewKeyPackage();
+                KeyType keyType = keyPackage.addNewKey();
+                keyType.setId(keyid.toString().replace("-", ""));
+                keyType.setAlgorithm("urn:dece:pskc:contentkey");
+                KeyDataType keyDataType = keyType.addNewData();
+                BinaryDataType binaryDataTypeSecret = keyDataType.addNewSecret();
+                EncryptedDataType encryptedDataType = binaryDataTypeSecret.addNewEncryptedValue();
+                encryptedDataType.addNewEncryptionMethod().setAlgorithm("http://www.w3.org/2001/04/xmlenc#rsa_1_5");
+                Cipher cipher = Cipher.getInstance("RSA");
+
+                cipher.init(Cipher.ENCRYPT_MODE, certificate);
+                CipherDataType cipherDataType = encryptedDataType.addNewCipherData();
+                cipherDataType.setCipherValue(cipher.doFinal(key.getEncoded()));
+
+                keyContainerDocument.save(new File(this.outputDirectory, certificate.getSubjectDN().getName() + ".pskcxml"));
+            }
+
+
+
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
 
     @Override
     protected void writeManifest(Map<String, List<Track>> trackFamilies, Map<Track, Long> trackBitrate, Map<Track, String> trackFilename, Map<Track, Container> dashedFiles) throws IOException {
