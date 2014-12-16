54c7b97bdb049c32c2a27174bb6206d84bd12f9a
Sebastian Annies <Sebastian.Annies@googlemail.com>
2014-12-16 10:36:03 +0100

    added a tool to sort tracks for further processing (castLabs/customer related work - most likely not usable for broad public)

--- dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/Main.java
+++ dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/Main.java
@@ -9,7 +9,7 @@ package com.castlabs.dash.dashfragmenter;
 import com.castlabs.dash.dashfragmenter.cmdlines.DashFileSet;
 import com.castlabs.dash.dashfragmenter.cmdlines.DashFileSetEncrypt;
 import com.castlabs.dash.dashfragmenter.cmdlines.MuxMp4;
-import com.castlabs.dash.dashfragmenter.cmdlines.Test;
+import com.castlabs.dash.dashfragmenter.cmdlines.SortTracks;
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
@@ -34,7 +34,7 @@ public class Main {
     @SubCommands({
             @SubCommand(name = "dash", impl = DashFileSet.class),
             @SubCommand(name = "encrypt", impl = DashFileSetEncrypt.class),
-            @SubCommand(name = "test", impl = Test.class),
+            @SubCommand(name = "sort", impl = SortTracks.class),
             @SubCommand(name = "mux", impl = MuxMp4.class)
     })
     Command command;
---
+++ dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/cmdlines/SortTracks.java
@@ -0,0 +1,133 @@
+package com.castlabs.dash.dashfragmenter.cmdlines;
+
+import com.castlabs.dash.dashfragmenter.Command;
+import com.castlabs.dash.dashfragmenter.ExitCodeException;
+import com.coremedia.iso.Hex;
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.IsoTypeReaderVariable;
+import com.coremedia.iso.boxes.Container;
+import com.coremedia.iso.boxes.TrackBox;
+import com.coremedia.iso.boxes.mdat.SampleList;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.Mp4TrackImpl;
+import com.googlecode.mp4parser.authoring.Sample;
+import com.googlecode.mp4parser.authoring.WrappingTrack;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.builder.Mp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
+import com.googlecode.mp4parser.boxes.mp4.samplegrouping.SampleToGroupBox;
+import com.googlecode.mp4parser.util.Path;
+import com.mp4parser.iso14496.part15.AvcConfigurationBox;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.control.TitledPane;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.FileOptionHandler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+import static com.googlecode.mp4parser.util.CastUtils.l2i;
+
+/**
+ * Created by sannies on 08.12.2014.
+ */
+public class SortTracks implements Command {
+
+    @Option(required = true, handler = FileOptionHandler.class, name = "--outputdir", aliases = "-o")
+    File outputDir;
+
+    @Argument(required = true, multiValued = true, handler = FileOptionHandler.class, usage = "Bitstream input files", metaVar = "vid1.avc1, aud1.dtshd ...")
+    protected List<File> inputFiles;
+
+
+    public int run() throws IOException, ExitCodeException {
+        Set<String> audiosDone = new HashSet<String>();
+        outputDir.mkdirs();
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        }
+        catch(NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        // collect videos
+        Mp4Builder builder = new DefaultMp4Builder();
+        for (File f : inputFiles) {
+            if (f.getName().endsWith(".mp4")) {
+                IsoFile isoFile = new IsoFile(f.getAbsolutePath());
+
+                if (Path.getPath(isoFile, "/moov[0]/trak[0]/mdia[0]/minf[0]/stbl[0]/stsd[0]/avc1[0]") != null) {
+                    AvcConfigurationBox avcC = Path.getPath(isoFile, "/moov[0]/trak[0]/mdia[0]/minf[0]/stbl[0]/stsd[0]/avc1[0]/avcC[0]");
+                    Mp4TrackImpl mp4Track = new Mp4TrackImpl(f.getName(), (TrackBox) Path.getPath(isoFile, "/moov[0]/trak[0]"));
+                    int fps = (int) ((double) mp4Track.getSamples().size() / ((double) mp4Track.getDuration() / (double) mp4Track.getTrackMetaData().getTimescale()));
+                    if (fps > 15) {
+
+                        long[] syncSamples = new long[0];
+
+                        for (int i = 1; i <= mp4Track.getSamples().size(); i++) {
+                            boolean idr = false;
+                            ByteBuffer s = mp4Track.getSamples().get(i - 1).asByteBuffer();
+                            while (s.remaining() > 0) {
+                                int length = l2i(IsoTypeReaderVariable.read(s, avcC.getLengthSizeMinusOne() + 1));
+                                ByteBuffer nal = ((ByteBuffer) s.slice().limit(length));
+                                int type = nal.get();
+                                int nal_ref_idc = (type >> 5) & 3;
+                                int nal_unit_type = type & 0x1f;
+                                idr |= nal_unit_type == 5;
+                                s.position(s.position() + length);
+                            }
+                            if (idr) {
+
+                                syncSamples = Arrays.copyOf(syncSamples, syncSamples.length + 1);
+                                syncSamples[syncSamples.length - 1] = i;
+                            }
+
+                        }
+                        Movie m = new Movie();
+                        m.addTrack(new WrappingTrack(mp4Track));
+                        Container correctedFile = builder.build(m);
+                        correctedFile.writeContainer(
+                                new FileOutputStream(new File(outputDir, f.getName())).getChannel());
+                        System.out.println("Added " + f.getName() + " with corrected sync samples.");
+
+
+                    }
+                }
+                if (Path.getPath(isoFile, "/moov[0]/trak[0]/mdia[0]/minf[0]/stbl[0]/stsd[0]/mp4a[0]") != null) {
+                    Mp4TrackImpl mp4Track = new Mp4TrackImpl(f.getName(), (TrackBox) Path.getPath(isoFile, "/moov[0]/trak[0]"));
+                    for (Sample sample : mp4Track.getSamples()) {
+                        md.update(sample.asByteBuffer());
+                    }
+                    String audioFingerPrint = Hex.encodeHex(md.digest());
+                    if (!audiosDone.contains(audioFingerPrint)) {
+                        if (!audiosDone.isEmpty()) {
+                            throw new RuntimeException("More than 1 audio. We need to pick up the languages somehow.");
+                        }
+                        audiosDone.add(audioFingerPrint);
+                        Movie m = new Movie();
+                        m.addTrack(mp4Track);
+                        Container correctedFile = builder.build(m);
+                        correctedFile.writeContainer(
+                                new FileOutputStream(new File(outputDir, f.getName())).getChannel());
+                        System.out.println("Added " + f.getName() + " as new audio.");
+                    }
+                }
+            }
+            if (f.getName().endsWith(".xml")) {
+                FileUtils.copyFile(f, new File(outputDir, f.getName()));
+                System.out.println("Added " + f.getName() + " as subtitle.");
+            }
+        }
+
+        return 0;
+    }
+}
