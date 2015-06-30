c839d9bf4b7e012e19f70ef7f51e298ee90bbb9e
Sebastian Annies <Sebastian.Annies@googlemail.com>
2015-06-30 14:42:30 +0200

    removed leftovers from dependenttrack research

--- dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/Main.java
+++ dash.fragmencrypter/src/main/java/com/castlabs/dash/dashfragmenter/Main.java
@@ -9,7 +9,6 @@ package com.castlabs.dash.dashfragmenter;
 import com.castlabs.dash.dashfragmenter.cmdlines.DashFileSet;
 import com.castlabs.dash.dashfragmenter.cmdlines.DashFileSetEncrypt;
 import com.castlabs.dash.dashfragmenter.cmdlines.MuxMp4;
-import com.castlabs.dash.dashfragmenter.representation.DependentTrackVariantA;
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
@@ -34,8 +33,8 @@ public class Main {
     @SubCommands({
             @SubCommand(name = "dash", impl = DashFileSet.class),
             @SubCommand(name = "encrypt", impl = DashFileSetEncrypt.class),
-            @SubCommand(name = "mux", impl = MuxMp4.class),
-            @SubCommand(name = "dash-dependent-track-a", impl = DependentTrackVariantA.class)
+            @SubCommand(name = "mux", impl = MuxMp4.class)
+
     })
     Command command;
 
